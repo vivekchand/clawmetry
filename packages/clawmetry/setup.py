4a27d9508db5d79e0433ccb554e70232cf42fa98
Vivek Chand <vivekchand19@gmail.com>
2026-02-16 18:59:10 +0100

    fix: ClawMetry branding in alias package setup.py

--- packages/clawmetry/setup.py
+++ packages/clawmetry/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="clawmetry",
     version="0.1.0",
-    description="Clawmetry alias package for OpenClaw Dashboard",
+    description="ClawMetry alias package for OpenClaw Dashboard",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Vivek Chand",
